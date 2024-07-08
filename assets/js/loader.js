7412811e1d07bb4e47ba091321a7fd6514382c46
Tadeo <tade25.molinas@gmail.com>
2024-07-08 10:56:07 -0400

    loader final

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $(window).on('load', function() {
-        // Hide the loader and show the page content
         $('#loader').fadeOut('slow', function() {});
     });
 });
